c24df70ea087140f2d23678eb6b68f741b341f83
Patrick Coffey <patrickcoffey48@gmail.com>
2017-06-17 15:19:59 -0500

    Progress on JSON API scaffolding

--- services/yodle-hostname/handler.js
+++ services/yodle-hostname/handler.js
@@ -3,7 +3,38 @@
  * Contains hostname API definition for Yodle.
  */
 
-const serverless = require('serverless-http');
+import express from 'express'
+import fortune from 'fortune'
+import serverless from 'serverless-http'
+import fortuneHTTP from 'fortune-http;
+import fortuneMongo from 'fortune-mongodb'
+import fortuneJsonAPI from 'fortune-json-api'
+
+// Initialize fortune instance (Define schema and store).
+const store = fortune({
+  hostname: {
+    name: String,
+  }
+}, {
+  adapter: [
+    fortuneMongo,
+    { url: 'mongodb://localhost:27017/test' }
+  ]
+})
+
+// Initialize an HTTP listener.
+const listener = fortuneHTTP(store, {
+  serializers: [
+    [ fortuneJsonAPI ]
+  ]
+})
+
+// Initialize app object.
+const app = express()
+app.use((request, response) =>
+  listener(request, response)
+  .catch(error => { ... }))
+
 
 module.exports.api = (event, context, callback) => {
   callback(null, {
