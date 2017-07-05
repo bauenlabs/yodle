2b80b55bcf67182c0abea55e70b2e51351ccdea8
Patrick Coffey <patrickcoffey48@gmail.com>
2017-07-04 22:48:07 -0500

    Switch back to serverless-webpack

--- services/yodle-hostname/handler.js
+++ services/yodle-hostname/handler.js
@@ -35,8 +35,14 @@ app.use((req, res) => {
   listener(req, res)
 })
 
-const server = awsServerlessExpress.createServer(app)
+// Initialize proxy server.
+const server = awsServerlessExpress.createServer(app, null, ['application/vnd.api+json'])
+
+// Export endpoint handler.
 module.exports.api = (event, context, callback) => {
+  // Stub the succeed method, map it to the callback.
   context.succeed = result => callback(null, result);
+
+  // Run the proxy.
   awsServerlessExpress.proxy(server, event, context)
 }
